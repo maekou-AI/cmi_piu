ed594ed14e5bca51b037aec9ec45f0c5fdb7e70f
maekou-AI <maeclipse1999@gmail.com>
2024-10-27 12:18:50 +0000

    new baseline file

---
+++ src/exp/exp005_lgb_baseline.py
@@ -0,0 +1,70 @@
+# Import
+import sys
+sys.path.append("/kaggle/src")  # Change on kaggle notebook
+
+from pathlib import Path
+
+import polars as pl
+from sklearn.preprocessing import LabelEncoder
+
+from common.constant import TRAIN_CSV_PATH, TEST_CSV_PATH, OUTPUT_DIR
+from run.lgb002 import run_kfold
+
+# Data
+trn_df = pl.read_csv(TRAIN_CSV_PATH)
+tst_df = pl.read_csv(TEST_CSV_PATH)
+
+base_cols = [col for col in trn_df.columns if col in tst_df.columns]
+df = pl.concat([trn_df[base_cols], tst_df])
+
+# Feature engineering
+features = pl.concat(
+    [
+        df,
+    ],
+    how="horizontal",
+)
+
+trn_id = trn_df.drop_nulls(subset=["sii"])["id"].to_list()
+tst_id = tst_df["id"].to_list()
+features = pl.concat([
+    features.slice(0, len(trn_df)).filter(pl.col("id").is_in(trn_id)),
+    features.slice(len(trn_df)).filter(pl.col("id").is_in(tst_id))
+])
+
+# Preprocessing
+cat_cols = features.select(pl.col(pl.Utf8)).columns
+cat_cols.remove("id")
+le = LabelEncoder()
+for col in cat_cols:
+    encoded = le.fit_transform(features[col].to_numpy())
+    features = features.with_columns(pl.Series(encoded).alias(col))
+
+# Model parameters
+model_params = {
+    "objective": "regression",
+    "metric": "rmse",
+    "boosting": "gbdt",
+    "num_leaves": 64,
+    "bagging_fraction": 0.8,
+    "bagging_freq": 1,
+    "feature_fraction": 0.8,
+    "learning_rate": 0.01,
+    "seed": 1,
+    "num_threads": 4,
+}
+
+save_dir = Path(OUTPUT_DIR) / Path(__file__).stem  # Change on kaggle notebook
+save_dir.mkdir(exist_ok=True, parents=True)
+
+run_kfold(
+    features=features.drop("id").to_pandas(),
+    trn_targets=trn_df.filter(pl.col("id").is_in(trn_id))["sii"].to_pandas(),
+    categorical_features=cat_cols,
+    n_splits=10,
+    save_dir=save_dir,
+    model_params=model_params,
+    weights=None,
+    trn_id=trn_id,
+    tst_id=tst_id,
+)
---
+++ src/run/lgb002.py
@@ -0,0 +1,129 @@
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
+from scipy.optimize import minimize
+from sklearn.metrics import cohen_kappa_score
+from sklearn.model_selection import StratifiedKFold
+
+
+NUM_BOOST_ROUND = 5000
+STOPPING_ROUNDS = 20
+VERBOSE_EVAL = 20
+
+
+def quadratic_weighted_kappa(preds, data):
+    y_true = data.get_label()
+    y_pred = preds.clip(0, 3).round().astype(int)
+    qwk = cohen_kappa_score(y_true, y_pred, weights="quadratic")
+    return "QWK", qwk, True
+
+
+def threshold_rounder(oof_preds, thresholds):
+    labels = []
+    for value in oof_preds:
+        if value < thresholds[0]:
+            labels.append(0)
+        elif value < thresholds[1]:
+            labels.append(1)
+        elif value < thresholds[2]:
+            labels.append(2)
+        else:
+            labels.append(3)
+    return np.array(labels)
+
+
+def evaluate_predictions(thresholds, y_true, oof_preds):
+    oof_preds_rounded = threshold_rounder(oof_preds, thresholds)
+    return -cohen_kappa_score(y_true, oof_preds_rounded, weights="quadratic")
+
+
+def train_kfold(features, targets, categorical_features, n_splits, model_params, weights=None):
+    if weights is None:
+        weights = np.ones(len(features))
+    skf = StratifiedKFold(
+        n_splits=n_splits, shuffle=True, random_state=1
+    )
+    boosters = []
+    losses = []
+    oof_preds = np.zeros(len(features))
+
+    for trn_idx, val_idx in skf.split(features, targets):
+        trn_data = lgb.Dataset(
+            data=features.iloc[trn_idx],
+            label=targets.iloc[trn_idx],
+            weight=weights[trn_idx],
+            categorical_feature=categorical_features,
+        )
+        val_data = lgb.Dataset(
+            data=features.iloc[val_idx],
+            label=targets.iloc[val_idx],
+            weight=weights[val_idx],
+            reference=trn_data,
+        )
+        eval_result = {}
+        callbacks = [
+            lgb.early_stopping(STOPPING_ROUNDS),
+            lgb.log_evaluation(VERBOSE_EVAL),
+            lgb.record_evaluation(eval_result),
+        ]
+        booster = lgb.train(
+            model_params,
+            trn_data,
+            valid_sets=[trn_data, val_data],
+            valid_names=["trn", "val"],
+            num_boost_round=NUM_BOOST_ROUND,
+            callbacks=callbacks,
+        )
+        boosters.append(booster)
+        losses.append(
+            eval_result["val"]["rmse"][booster.best_iteration - 1]
+        )
+        oof_preds[val_idx] = booster.predict(features.iloc[val_idx])
+    print(f"Avg. RMSE: {np.mean(losses):.4f}")
+    return boosters, eval_result, losses, oof_preds
+
+
+def run_kfold(
+        features, trn_targets, categorical_features, n_splits, model_params, save_dir, 
+        weights=None, trn_id=None, tst_id=None
+):
+    trn_features = features.iloc[:len(trn_targets)]
+    tst_features = features.iloc[len(trn_targets):]
+    boosters, eval_result, qwks, oof_preds = train_kfold(
+        trn_features, trn_targets, categorical_features, n_splits, model_params, weights
+    )
+    # save_loss
+    loss = pd.DataFrame(
+        data={
+            "trn_rmse": eval_result["trn"]["rmse"],
+            "val_rmse": eval_result["val"]["rmse"],
+        }
+    )
+    loss.to_csv(save_dir / "log_metric.csv", index=False)
+    # kappa_optimizer
+    kappa_optimizer = minimize(
+        evaluate_predictions,
+        x0=[0.5, 1.5, 2.5],
+        args=(trn_targets.to_numpy(), oof_preds),
+        method="Nelder-Mead",
+    )
+    # oof_preds
+    oof_preds = threshold_rounder(oof_preds, kappa_optimizer.x)
+    qwk_optimized = cohen_kappa_score(trn_targets.to_numpy(), oof_preds, weights="quadratic")
+    print(f"Optimized Kappa: {qwk_optimized:.4f}")
+    oof_preds = pd.DataFrame(
+        data={"id": trn_id, "sii": oof_preds}
+    )
+    oof_preds.to_csv(save_dir / "oof_prediction.csv", index=False)
+    (save_dir / "score.txt").write_text(f"Kappa: {str(np.mean(qwks))}, Optimized Kappa: {str(qwk_optimized)}")
+    # tst_preds
+    tst_preds = np.zeros(len(tst_features))
+    for i, booster in enumerate(boosters):
+        tst_preds += booster.predict(tst_features) / n_splits
+        booster.save_model(str(save_dir / f"booster.{i}.txt"))
+    tst_preds = threshold_rounder(tst_preds, kappa_optimizer.x)
+    tst_preds = pd.DataFrame(
+        data={"id": tst_id, "sii": tst_preds}
+    )
+    tst_preds.to_csv("/kaggle/working/submission.csv", index=False)
+    
